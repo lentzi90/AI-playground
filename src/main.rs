aa384a5fc0f3269a82288ccb9d11885f21022cc5
Lennart Jern <lennart.jern@gmail.com>
2016-11-01 22:13:00 +0100

    Some progress...
    - A simple game struct
    - JSON encoding of struct

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 extern crate rand;
+// extern crate time;
+
+extern crate rustc_serialize;
+use rustc_serialize::json;
 
 use iron::prelude::*;
 use iron::status;
@@ -11,11 +15,40 @@ use mount::Mount;
 use staticfile::Static;
 use std::path::Path;
 
-const HEIGHT: i32 = 480;
-const WIDTH: i32 = 512;
-const BOUNDARY: i32 = 32;
+use std::time::{Duration, Instant};
+use std::thread::sleep;
+
+const HEIGHT: u64 = 480;
+const WIDTH: u64 = 512;
+const BOUNDARY: u64 = 32;
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Game {
+    hero: Point,
+    gnome: Point,
+    speed: u64,
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Point {
+    x: u64,
+    y: u64,
+}
 
 fn main() {
+
+    let mut game = Game {
+        hero: Point {x: WIDTH/2, y: HEIGHT/2},
+        gnome: Point {x: 3, y: 4},
+        speed: 256,
+    };
+
+    let time = Instant::now();
+    let encoded = json::encode(&game).unwrap();
+    println!("{:?}", encoded);
+    game.update(time);
+    let encoded = json::encode(&game).unwrap();
+    println!("Updated: {:?}", encoded);
 
     // Handle get requests by sending back a random position on the map in json format.
     fn handler(_request: &mut Request) -> IronResult<Response> {
@@ -38,4 +71,13 @@ fn main() {
     // Serve game server at /
     mount.mount("/", handler);
     Iron::new(mount).http("127.0.0.1:4000").unwrap();
+}
+
+impl Game {
+    fn update(&mut self, time: Instant) {
+        sleep(Duration::new(2, 0));
+        let delta_t = time.elapsed().as_secs();
+        let delta_x = self.speed * delta_t;
+        self.hero.x = self.hero.x + delta_x;
+    }
 }
