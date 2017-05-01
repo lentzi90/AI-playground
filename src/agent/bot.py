552e15117b50753ea248efe62d59acf72e9ce048
Lennart Jern <lennart.jern@gmail.com>
2017-05-01 11:41:21 +0200

    Work on simple-agent and added bot
    - The simple-agent is not working yet
    - The bot is a very simple "not smart" bot that know how to beat the game

--- src/agent/bot.py
+++ src/agent/bot.py
@@ -17,28 +17,12 @@ DIRECTIONS = {"Left": 2*pi/2, "Right": 2*pi, "Up": 2*pi*3/4, "Down": 2*pi/4}
 
 class UnexpectedResponse(Exception): pass
 
-class Perceptron:
-    """
-    A Perceptron or neuron that reacts with a certain activity level depending on input
-    """
-
-    def __init__(self, action):
-        self.action = action
-        self.weights = np.ones(shape=(2,4))
-
-    def activity(self, observation):
-        """Calculate the activity based on the given image."""
-        result = np.sum(np.multiply(observation, self.weights))
-        act = 1/(1+np.exp(-result))
-        return act
-
 class Brain:
     """
     A Brain has 4 different Perceptrons, one for each possible action.
     """
 
     def __init__(self):
-        self.perceptrons = [Perceptron(a) for a in ACTIONS]
         self.memory = np.ones(shape=(2,4))
         self.last_action = ACTIONS[0]
         self.last_activities = np.zeros(4)
@@ -66,27 +50,26 @@ class Brain:
         self.old_memory = self.memory
         self.memory = observation
 
-        # Get reward and update Perceptrons
-        reward = get_reward(data)
-        self.teach_perceptrons(reward)
-
 
     def make_decision(self):
         """React to an observation."""
 
-        # Execute random actions every now and then
-        if random() > 0.87:
-            index = randrange(0, 4)
-            return self.perceptrons[index].action
+        action = ""
 
-        # Get the activity of every perceptron
-        activities = [p.activity(self.memory) for p in self.perceptrons]
+        x_dir = self.memory[0][2] - self.memory[0][0]
+        y_dir = self.memory[0][3] - self.memory[0][1]
 
-        # Add activity levels to memory
-        self.last_activities = activities
-        # Find the max level
-        index = np.argmax(activities)
-        action = self.perceptrons[index].action
+        if (abs(x_dir) < abs(y_dir)):
+            if (y_dir < 0):
+                action = "Up"
+            else:
+                action = "Down"
+        else:
+            if (x_dir < 0):
+                action = "Left"
+            else:
+                action = "Right"
+        
         self.last_action = action
         return action
 
@@ -106,23 +89,6 @@ class Brain:
         else:
             raise UnexpectedResponse(response)
 
-    def teach_perceptrons(self, reward):
-        """Update the weights based on the reward."""
-        performance = reward - self.last_reward
-        responsible = next(p for p in self.perceptrons if p.action == self.last_action)
-        delta = performance*self.old_memory
-        responsible.weights = responsible.weights + delta*self.learning_rate
-        responsible.weights = np.minimum(responsible.weights, 1)
-
-        if (performance < 0):
-            for p in self.perceptrons:
-                if p.action != self.last_action:
-                    p.weights = p.weights - delta*self.learning_rate
-                    p.weights = np.minimum(p.weights, 1)
-
-        # Update last reward
-        self.last_reward = reward
-
 def get_data():
     """Reads the current state of the world"""
     server = http.client.HTTPConnection(URL)
@@ -135,17 +101,6 @@ def get_data():
     else:
         return UnexpectedResponse(response)
 
-def get_reward(data):
-    """The reward is based on how close the hero is to the gnome"""
-    hx = data["hero"]["x"]
-    hy = data["hero"]["y"]
-    gx = data["gnome"]["x"]
-    gy = data["gnome"]["y"]
-
-    distance = sqrt((gx-hx)**2 + (gy-hy)**2)
-    reward = 100/(distance+100) # max 1
-    return reward
-
 
 brain = Brain()
 print("Initialized brain")
@@ -155,4 +110,3 @@ while True:
     act = brain.make_decision()
     brain.execute_action(act)
     time.sleep(0.2)
-    print(brain.perceptrons[0].weights)
